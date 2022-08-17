6c8098ca1542f7c7ab27f6765c7157b92475e884
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-08-17 21:39:01 +0700

    update membersdata.kt

--- app/src/main/java/com/hendra/treasure/MembersData.kt
+++ app/src/main/java/com/hendra/treasure/MembersData.kt
@@ -1,31 +1,31 @@
 package com.hendra.treasure
 
 object MembersData {
-    private val memberNames = arrayOf("Choi Hyunsuk",
-        "Park Jihoon",
-        "Yoshi",
-        "Kim Junkyu",
-        "Mashiho",
-        "Yoon Jaehyuk",
-        "Asahi",
-        "Bang Yedam",
-        "Kim Doyoung",
-        "Haruto",
-        "Park Jeongwoo",
-        "Song Junghwan")
-
-    private val memberDetails = arrayOf("Choi Hyunsuk (최현석)",
-        "Park Ji Hoon (박지훈)",
+    private val memberNames = arrayOf("Choi Hyunsuk (최현석)",
+        "Park Jihoon (박지훈)",
         "Kanemoto Yoshinori (金本芳典)",
         "Kim Jun Kyu (김준규)",
         "Takata Mashiho (高田ましほ)",
         "Yoon Jae Hyuk (윤재혁)",
         "Hamada Asahi (浜田朝光)",
-        "Bang Ye Dam (방예담)",
-        "Kim Do Young (김도영)",
+        "Bang Yedam (방예담)",
+        "Kim Doyoung (김도영)",
         "Watanabe Haruto (渡辺春虎)",
-        "Park Jeong Woo (박정우)",
-        "So Jung Hwan (소정환)")
+        "Park Jeongwoo (박정우)",
+        "Song Jung Hwan (소정환)")
+
+    private val memberDetails = arrayOf("Birth Name: Choi Hyun Suk (최현석) English Name: Daniel Choi Possible Position: Leader, Main Rapper, Main Dancer Birthday: April 21st, 1999 Zodiac Sign: Taurus Height: 169 cm (5’6.5″) Weight: 58 kg (128 lbs)",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "")
 
     private val membersImages = intArrayOf(R.drawable.choi_hyunsuk,
         R.drawable.park_jihoon,
