27033c49c967b6377057d96422de18f51bcf431c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-08-17 19:48:08 +0700

    Menambahkan Logic RecyclerView

--- app/src/main/java/com/hendra/treasure/MemberTreasure.kt
+++ app/src/main/java/com/hendra/treasure/MemberTreasure.kt
@@ -6,6 +6,28 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MemberTreasure : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private var list: ArrayList<Member> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_member_treasure)
+
+        recyclerView = findViewById(R.id.rv_members)
+        recyclerView.setHasFixedSize(true)
+
+        list.addAll(MembersData.listData)
+        showRV()
+    }
+
+    private fun showRV() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = CardViewMemberAdapter(list)
+        recyclerView.adapter = listHeroAdapter
+    }
 }
