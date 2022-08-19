31b77adbb583d2fcb6429b0888e9e1282e6763d6
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-08-19 14:04:38 +0700

    update detailactivity.kt

--- app/src/main/java/com/hendra/treasure/DetailActivity.kt
+++ app/src/main/java/com/hendra/treasure/DetailActivity.kt
@@ -7,5 +7,7 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        supportActionBar?.title = "Detail Member"
     }
 }
